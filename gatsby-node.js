8c4e595e5792be6a33ed020bccab9382e2240670
jmshanahan <comeraghsolutions@gail.com>
2020-05-21 15:51:21 +0100

    Initial Commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,26 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allBook {
+        edges {
+          next {
+            id
+          }
+          node {
+            summary
+            title
+            id
+            author {
+              name
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+  })
+}
